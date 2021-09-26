6a8270322c1bf75ae8f48c7f85032f9717b2dfcc
tony202123497 <91227036+tony202123497@users.noreply.github.com>
2021-09-25 19:03:56 -0500

    Dispensador V3
    Incluye análiticas

--- Dispensador.java
+++ Dispensador.java
@@ -1,66 +1,337 @@
 package Proyecto;
 
-//Avance en proceso, se puede añadir comentarios con éxtio//
-import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Dispensador {
 
-    public static void main(String[] args) {
+    //ANALITICAS:
+    //Producto más vendido
+    //Venta en soles
+    //Venta en ecomoneda
+    //... Productos con quiebre de stock
+    //... Total de venta por producto
+    //... Ventas de categoria mascarillas
 
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    private static final String[] descripcion = {"Gel-Antibacterial","Jabon Liquido","Mascarilla Kid","Cubre Zapato","Mini Alcohol",
+            "Lentes De Seguridad Blancos","Mascarilla K95","Mascarilla Adulto","Guantes Quirugicos","Cubre Cabeza"};
+    private static final double[] costoSoles = {4.0, 5.0, 2.0, 1.5, 2.5, 3.5, 2.5, 2.0, 1.5, 1.5};
+    private static final int[] costoEcomoneda = {120, 150, 60, 45, 75, 105, 75, 60, 45, 45};
+    private static final int[] stock = {4, 4, 4, 4, 4, 4, 4, 4, 4, 0};
+
+    private static int[] ventaUnidades = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private static double[] ventaSoles = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    private static int[] ventaEcomoneda = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
         boolean salir = false;
         int opcion; //Guardaremos la opcion del usuario
 
         while (!salir) {
-            System.out.println("=========================================");
-            System.out.println("Bienvenido al dispensador ecoamigable");
-            System.out.println("=========================================");
-            System.out.println("1. Listado de productos");
-            System.out.println("2. Comprar");
-            System.out.println("3. Salir");
-            System.out.println("=========================================");
+            listadoDeProductos(); //pintar el menu principal
+            opcion = sn.nextInt(); //leer la opcion digitada por el usuario
 
-            try {
+            switch (opcion) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    if(stock[opcion] > 0){
+                        procesarPago(opcion);
+                    }else{
+                        stockInsuficiente();
+                    }
+                    break;
+                case 98:
+                    analiticas(); //pinta el menu de analiticas
+                    break;
+                case 99:
+                    salir = true;
+                    break;
+                default:
+                    opcionNoValida(); //pinta opción no válida
+                    break;
+            }
+        }
+    }
 
-                System.out.print("Escribe una de las opciones ");
-                opcion = sn.nextInt();
+    private static void analiticas() {
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcionAnalitica;
 
-                switch (opcion) {
-                    case 1:
-                        listadoDeProductos();
-                        break;
-                    case 2:
-                        System.out.println("Has seleccionado la opción 2");
-                        break;
-                    case 3:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println("Solo números entre 1 y 3");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Debes insertar un número");
-                sn.next();
+        while(!salir)
+        {
+            System.out.println("========================================");
+            System.out.println("               ANALITICAS               ");
+            System.out.println("========================================");
+            System.out.println("[ 1] Producto más vendido");
+            System.out.println("[ 2] Venta por forma de pago");
+            System.out.println("[ 3] Productos con quiebre de stock");
+            System.out.println("[ 4] Venta por producto");
+            System.out.println("[ 5] Venta de mascarillas (kid, adulto, kn95)");
+            System.out.println("[99] Salir");
+            System.out.print("Elija una opción: ");
+            opcionAnalitica = sn.nextInt();
+
+            switch(opcionAnalitica)
+            {
+                case 1:
+                    productoMasVendido();
+                    break;
+                case 2:
+                    ventaFormaDePago();
+                    break;
+                case 3:
+                    productosSinStock();
+                    break;
+                case 4:
+                    ventaPorProducto();
+                    break;
+                case 5:
+                    ventaMascarillas();
+                    break;
+                case 99:
+                    salir = true;
+                    break;
+                default:
+                    opcionNoValida();
+                    break;
+            }
+        }
+    }
+
+    private static void ventaMascarillas() {
+
+    }
+
+    private static void ventaPorProducto() {
+
+    }
+
+    private static void productosSinStock() {
+        int cantidad = 0;
+
+        System.out.println(ANSI_BLUE + "========================================");
+        System.out.println("         PRODUCTOS SIN STOCK");
+        System.out.println("========================================");
+
+        for(int i=0; i < stock.length; i++)
+        {
+            if(stock[i] == 0)
+            {
+                cantidad = cantidad + 1;
+                System.out.println("[" + cantidad + "] " + descripcion[i].toUpperCase(Locale.ROOT));
+            }
+        }
+        System.out.println("TOTAL: " + cantidad + " PRODUCTO(S) SIN STOCK." + ANSI_RESET);
+        continuar();
+    }
+
+    private static void ventaFormaDePago() {
+        int cantVtaSoles = 0, cantVtaEcomoneda = 0, cantTotalVtas = 0;
+        double montoVtaSoles = 0;
+        int montoVtaEcomoneda = 0;
+
+        for(int i = 0; i < ventaSoles.length; i++)
+        {
+            if(ventaSoles[i] > 0){
+                montoVtaSoles = montoVtaSoles + ventaSoles[i];
+                cantVtaSoles = cantVtaSoles + 1;
+                cantTotalVtas = cantTotalVtas + 1;
+            }
+
+            if(ventaEcomoneda[i] > 0){
+                montoVtaEcomoneda = montoVtaEcomoneda + ventaEcomoneda[i];
+                cantVtaEcomoneda = cantVtaEcomoneda + 1;
+                cantTotalVtas = cantTotalVtas + 1;
             }
         }
 
+        System.out.println(ANSI_BLUE + "========================================");
+        System.out.println("         VENTA POR FORMA DE PAGO");
+        System.out.println("========================================");
+        System.out.println("SOLES");
+        System.out.println("========================================");
+        System.out.println("Cantidad de ventas: " + cantVtaSoles);
+        if(cantTotalVtas > 0)
+            System.out.println("% de ventas       : " + ((cantVtaSoles/cantTotalVtas)*100.00) + "%");
+        System.out.println("Monto             : " + montoVtaSoles + " soles");
+        System.out.println("========================================");
+        System.out.println("ECOMONEDA");
+        System.out.println("========================================");
+        System.out.println("Cantidad de ventas: " + cantVtaEcomoneda);
+        if(cantTotalVtas > 0)
+            System.out.println("% de ventas       : " + ((cantVtaEcomoneda/cantTotalVtas)*100.00) + "%");
+        System.out.println("Monto             : " + montoVtaEcomoneda + " tapas");
+        System.out.println("========================================" + ANSI_RESET);
+        continuar();
+    }
+
+    private static void productoMasVendido() {
+        int cantMayor, posMayor;
+
+        posMayor = -1;
+        cantMayor = 0;
+        for(int i = 0; i < ventaUnidades.length; i++) {
+            if(cantMayor < ventaUnidades[i]) {
+                cantMayor = ventaUnidades[i];
+                posMayor = i;
+            }
+        }
+
+        if(posMayor != -1)
+        {
+            System.out.println(ANSI_BLUE + "========================================");
+            System.out.println("          PRODUCTO MAS VENDIDO          ");
+            System.out.println("========================================");
+            System.out.println(descripcion[posMayor].toUpperCase(Locale.ROOT) + " CON " + ventaUnidades[posMayor] + " UNIDAD(ES)." + ANSI_RESET);
+            continuar();
+        }
+        else{
+            System.out.println(ANSI_BLUE + "========================================");
+            System.out.println("        NO HAY PRODUCTOS VENDIDOS       ");
+            System.out.println("========================================" + ANSI_RESET);
+            continuar();
+        }        
+    }
+
+    private static void stockInsuficiente() {
+        System.out.println(ANSI_RED + "========================================");
+        System.out.println("         PRODUCTO NO DISPONIBLE         ");
+        System.out.println("========================================" + ANSI_RESET);
+        continuar();
+    }
+
+    private static void procesarPago(int opcion) {
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcionPago;
+        double importeSoles;
+        int importeEcomoneda;
+
+        while(!salir) {
+            System.out.println(ANSI_BLUE + "========================================");
+            System.out.println("SELECCIONADO: " + descripcion[opcion].toUpperCase(Locale.ROOT));
+            System.out.println("IMPORTE SOLES: S/. " + costoSoles[opcion]);
+            System.out.println("IMPORTE ECOMONEDA: " + costoEcomoneda[opcion]);
+            System.out.println("========================================" + ANSI_RESET);
+            System.out.println("Ingrese la forma de pago");
+            System.out.println("[ 1] Soles");
+            System.out.println("[ 2] Ecomoneda");
+            System.out.println("[99] Volver");
+            System.out.print("Elija una opción: ");
+            opcionPago = sn.nextInt();
+
+            switch (opcionPago) {
+                case 1:
+                    System.out.print("Ingrese el importe en soles: ");
+                    importeSoles = sn.nextDouble();
+                    if (importeSoles >= costoSoles[opcion]) {
+                        registrarVenta(opcion, opcionPago);
+                        detalleVenta(opcion, importeSoles, 0);
+                    } else {
+                        saldoInsuficiente();
+                    }
+                    salir = true;
+                    break;
+                case 2:
+                    System.out.print("Ingrese la cantidad de tapas: ");
+                    importeEcomoneda = sn.nextInt();
+                    if (importeEcomoneda >= costoEcomoneda[opcion]) {
+                        registrarVenta(opcion, opcionPago);
+                        detalleVenta(opcion, 0.0, importeEcomoneda);
+                    } else {
+                        saldoInsuficiente();
+                    }
+                    salir = true;
+                    break;
+                case 99:
+                    salir = true;
+                    break;
+                default:
+                    opcionNoValida();
+                    break;
+            }
+        }
+    }
+
+    private static void detalleVenta(int opcion, double importeSoles, int importeEcomoneda) {
+        System.out.println(ANSI_GREEN + "========================================");
+        System.out.println("            RESUMEN DE VENTA            ");
+        System.out.println("========================================");
+        System.out.println("PRODUCTO     : " + descripcion[opcion].toUpperCase(Locale.ROOT));
+        if(importeSoles > 0){
+            System.out.println("FORMA DE PAGO: SOLES");
+            System.out.println("IMPORTE PAGO : " + importeSoles);
+            System.out.println("VUELTO       : " + (importeSoles - costoSoles[opcion]));
+        }else{
+            System.out.println("FORMA DE PAGO: ECOMONEDA");
+            System.out.println("IMPORTE PAGO : " + importeEcomoneda);
+            System.out.println("DEVUELTO     : " + (importeEcomoneda - costoEcomoneda[opcion]));
+        }
+        System.out.println("========================================" + ANSI_RESET);
+        continuar();
+    }
+
+    private static void saldoInsuficiente() {
+        System.out.println(ANSI_RED + "========================================");
+        System.out.println("           SALDO INSUFICIENTE           ");
+        System.out.println("========================================" + ANSI_RESET);
+        continuar();
+    }
+
+    private static void registrarVenta(int opcion, int opcionPago) {
+        stock[opcion] = stock[opcion] - 1;
+        ventaUnidades[opcion] = ventaUnidades[opcion] + 1;
+        switch(opcionPago){
+            case 1:
+                ventaSoles[opcion] = ventaSoles[opcion] + costoSoles[opcion];
+                break;
+            case 2:
+                ventaEcomoneda[opcion] = ventaEcomoneda[opcion] + costoEcomoneda[opcion];
+                break;
+        }
     }
 
     private static void listadoDeProductos() {
-        System.out.println("=========================================");
-        System.out.println("Listado de productos");
-        System.out.println("=========================================");
-        System.out.println("1. Mascarilla\tS/. 1.50 soles\teco 15 tapas");
-        System.out.println("2. Facial    \tS/. 3.00 soles\teco 30 tapas");
-        System.out.println("2. Facial    \tS/. 3.00 soles\teco 30 tapas");
-        System.out.println("2. Facial    \tS/. 3.00 soles\teco 30 tapas");
-        System.out.println("2. Facial    \tS/. 3.00 soles\teco 30 tapas");
-        System.out.println("2. Facial    \tS/. 3.00 soles\teco 30 tapas");
-        
-        //Comentario de Prueba
+        System.out.println("========================================");
+        System.out.println("     BIENVENIDO A LA ECOEXPENDEDORA     ");
+        System.out.println("========================================");
 
+        for(int i=0; i < descripcion.length; i++)
+        {
+            System.out.println("[ " + i + "] " + descripcion[i] + " (S/. " + costoSoles[i] + " - Ecomoneda " + costoEcomoneda[i] + ")");
+        }
+        System.out.println("[98] Análiticas");
+        System.out.println("[99] Salir");
+        System.out.print("Escribe una de las opciones ");
     }
 
-}
+    private static void opcionNoValida(){
+        System.out.println(ANSI_RED + "========================================");
+        System.out.println("            OPCIÓN NO VÁLIDA             ");
+        System.out.println("========================================" + ANSI_RESET);
+        continuar();
+    }
 
+    private static void continuar(){
+        Scanner sn = new Scanner(System.in);
+        String entrada;
+
+        System.out.print("Presione enter para continuar.");
+        entrada = sn.nextLine();
+    }
+}
